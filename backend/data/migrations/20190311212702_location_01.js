df85b8b69a33594f789b5981a36a643852aa2b61
griffinshuth <griffinshuth@gmail.com>
2019-03-11 21:44:25 -0600

    added

---
+++ backend/data/migrations/20190311212702_location_01.js
@@ -0,0 +1,23 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('location', location => {
+        location.increments();
+        location
+        .string('location_street1', 128)
+        .notNullable();
+        location
+        .string('location_street2', 128);
+        location
+        .string('city', 128)
+        .notNullable();
+        location
+        .string('state', 128)
+        .notNullable();
+        location
+        .string('zipcode', 128)
+        .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('location');
+};
