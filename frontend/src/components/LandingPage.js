bedb85a3496a51a9821c44857e23abb7c8ee7f62
Alex Martin <atonymartin20@yahoo.com>
2019-03-12 21:09:15 -0500

    Removed testing things in LandingPage.js

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
-import TeamCard from './TeamCardList/TeamCard';
 
 class LandingPage extends Component {
   componentDidMount() {
