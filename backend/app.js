776c437844f2e0f71f5ba90c470cce9ba0a9d6b1
Jesse Anderson <janders9009@gmail.com>
2019-03-12 21:00:25 -0500

    cleaning up endpoint

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,6 @@ const authRouter = require('./routers/authRouter');
 app.use(express.json());
 app.use(cors());
 
-const getWeather = (latitude, longitude) => {
-  //   console.log(
-  //     'inside getWeather',
-  //     'latitude',
-  //     position.coords.latitude,
-  //     'longitude',
-  //     position.coords.longitude
-  //   );
-};
-
 const PORT = process.env.PORT || '4000';
 
 app.use('/auth', authRouter);
@@ -26,13 +16,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/weather', async (req, res) => {
-  const latitude = 43.546;
-  const longitude = -92.09;
+  const { latitude, longitude } = req.body;
   const apiKey = process.env.DS_API_KEY || null;
   axios
     .get(`https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}`)
     .then(response => {
-      console.log(response.data);
       res.send(response.data);
     })
     .catch(err => {
