a0cb420ae6c1f788033589fdd52423bcbff8a599
Jesse Anderson <janders9009@gmail.com>
2019-03-15 13:24:47 -0500

    minor change to styling of the user settings container to make it mobile responsive

--- frontend/src/components/UserSettings/UserSettings.js
+++ frontend/src/components/UserSettings/UserSettings.js
@@ -25,8 +25,8 @@ const styles = theme => ({
     maxWidth: 640,
     margin: '65px auto 0 auto',
     padding: '40px 20px 100px 20px',
-    height: 'calc(100vh - 65px)',
-    minHeight: 520
+    minHeight: 'calc(100vh - 65px)'
+    // minHeight: '100%'
   },
   formControl: {
     marginLeft: theme.spacing.unit,
