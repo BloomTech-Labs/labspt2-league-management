28a4f0a3268a2fd670256c03917f413624ae550e
Jesse Anderson <janders9009@gmail.com>
2019-03-15 00:27:17 -0500

    added the /settings endpoint to the app.js backend file. Cleaned up the routing of the app as a whole, and connected the settings page to the backend. The settings page is populated with data pulled from the database.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 const authRouter = require('./routers/authRouter');
+const settingsRouter = require('./routers/settingsRouter');
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(cors());
 const PORT = process.env.PORT || '4000';
 
 app.use('/auth', authRouter);
+app.use('/settings', settingsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send(`API active on port: ${PORT}`);
