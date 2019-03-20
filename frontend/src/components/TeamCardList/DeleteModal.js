84c06c89b5d4fa5310c65c0a87689aeec5b5ead4
Alex Martin <atonymartin20@yahoo.com>
2019-03-19 19:53:26 -0500

    Started creating DeleteModal

---
+++ frontend/src/components/TeamCardList/DeleteModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none'
+  }
+});
+
+class DeleteModal extends React.Component {
+  state = {};
+}
+
+DeleteModal.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+const DeleteModalWrapped = withStyles(styles)(DeleteModal);
+export default DeleteModalWrapped;
