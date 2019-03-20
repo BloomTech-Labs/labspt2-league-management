9acdd31db35873ff77490125558761c9dd607ca8
hraya <hraya@lssu.edu>
2019-03-19 21:09:15 -0500

    Deleted get all model

--- backend/data/models/leagueModel.js
+++ backend/data/models/leagueModel.js
@@ -1,10 +1,6 @@
 const db = require('../dbConfig.js');
 
 module.exports = {
-    getAll: () =>{
-        return db('league')
-    },
-
     getLeaguesByUser: (user) =>{
         return db('league').where('admin_user_id', user.id)
     },
