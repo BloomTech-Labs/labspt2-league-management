f1c16f7c4361dc92e000eba78230691932889642
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-03-21 10:51:47 -0600

    Refactored migrations and created seeds

--- backend/data/migrations/20190311202347_team_01.js
+++ backend/data/migrations/20190311202347_team_01.js
@@ -1,44 +1,37 @@
-exports.up = function (knex, Promise) {
-    return knex.schema.createTable('team', team => {
-        team.increments();
-        team
-            .string('name', 128)
-            .notNullable()
-        team
-            .integer('league_id')
-            .unsigned()
-            .notNullable();
-        team
-            .foreign('league_id')
-            .references('id')
-            .on('league');
-        team
-            .boolean('link_coach')
-            .defaultTo(false);
-        team
-            .integer('coach_user_id')
-            .unsigned()
-        team
-            .foreign('coach_user_id')
-            .references('id')
-            .on('users')
-        team
-            .string('coach_name', 128)
-        team
-            .string('coach_email', 128)
-        team
-            .string('coach_phone_number', 128);
-        team
-            .integer('wins')
-            .unsigned();
-        team
-            .integer('losses')
-            .unsigned();
-        team
-            .integer('ties')
-            .unsigned();
-    });
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('team', team => {
+    team.increments();
+    team.string('name', 128).notNullable();
+    team
+      .integer('league_id')
+      .unsigned()
+      .notNullable();
+    team
+      .foreign('league_id')
+      .references('id')
+      .on('league');
+    team.integer('coach_user_id').unsigned();
+    team
+      .foreign('coach_user_id')
+      .references('id')
+      .on('users');
+    team.string('coach_name', 128);
+    team.string('coach_email', 128);
+    team.string('coach_phone', 128);
+    team
+      .integer('wins')
+      .unsigned()
+      .defaultTo(0);
+    team
+      .integer('losses')
+      .unsigned()
+      .defaultTo(0);
+    team
+      .integer('ties')
+      .unsigned()
+      .defaultTo(0);
+  });
 };
-exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists('team');
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('team');
 };
