732cf32a247d4026b8fd7c020771d31f1ca0ed23
Jesse Anderson <janders9009@gmail.com>
2019-03-20 21:52:51 -0500

    creating CreateLeague component

---
+++ frontend/src/components/CreateLeague/ChooseLeague.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+};
+
+function ChooseLeague(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Basic League
+        </Typography>
+        <Typography variant="h5" component="h2">
+          League Description
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+ChooseLeague.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ChooseLeague);
