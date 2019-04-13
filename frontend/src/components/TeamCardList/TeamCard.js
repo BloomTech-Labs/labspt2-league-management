4493fda8da216cbc563401e1854db34105f55a53
Alex Martin <atonymartin20@yahoo.com>
2019-04-13 09:23:15 -0500

    Added Upcoming games to Team Cards

--- frontend/src/components/TeamCardList/TeamCard.js
+++ frontend/src/components/TeamCardList/TeamCard.js
@@ -84,8 +84,21 @@ class TeamCard extends React.Component {
     ties: this.props.ties,
     isFlipped: false,
     containsTies: false,
+    teamSchedule: [],
+    scheduleGame1: {
+      game1Month: null,
+      game1Day: null,
+      game1Time: null,
+      game1Opp: null
+    },
+    scheduleGame2: {
+      game2Month: null,
+      game2Day: null,
+      game2Time: null,
+      game2Opp: null
+    }
   };
-  
+
   ClickHandler = event => {
     event.preventDefault();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
@@ -112,23 +125,175 @@ class TeamCard extends React.Component {
       ties: this.state.ties
     };
     this.context.editTeamInLeague(teamData, this.props.index, this.state.id);
-    if(this.state.ties > 0) {
-      this.setState({ 
+    if (this.state.ties > 0) {
+      this.setState({
         containsTies: true
-      })
-    };
+      });
+    }
   };
 
-  componentDidMount() {
-    if(this.state.ties > 0) {
-      this.setState({ 
-        containsTies: true
-      })
+  getTeamSchedule() {
+    const lid = this.context.state.leagues[this.props.index].id;
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    if (month < 10) {
+      month = '0' + month;
+    }
+    let day = today.getDate();
+    if (day < 10) {
+      day = '0' + day;
+    }
+    let date = year + '-' + month + '-' + day;
+    let hours = today.getHours();
+    let time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+    let dateTime = date + 'T' + time;
+    const teamSchedule = [];
+    if (this.context.state.schedule_by_league.find(x => x.league_id === lid)) {
+      const leagueSchedule = this.context.state.schedule_by_league.find(
+        x => x.league_id === lid
+      ).games;
+      for (let i = 0; i < leagueSchedule.length; i++) {
+        if (leagueSchedule[i].home_team_id === this.state.id) {
+          teamSchedule.push(leagueSchedule[i]);
+        }
+        if (leagueSchedule[i].away_team_id === this.state.id) {
+          teamSchedule.push(leagueSchedule[i]);
+        }
+      }
+    }
+    for (let j = 0; j < teamSchedule.length; j++) {
+      if (Date.parse(teamSchedule[j].start_time) < Date.parse(today)) {
+        let removedGames = teamSchedule.splice(j, 1);
+      }
+    }
+    this.setState({
+      teamSchedule
+    });
+  }
+
+  displayGames() {
+    let editedTeamSchedule = this.state.teamSchedule;
+    const lid = this.context.state.leagues[this.props.index].id;
+    editedTeamSchedule.length = 2;
+    let game1Opp;
+    // Opponent Testing
+    if (this.context.state.teams_by_league.find(x => x.league_id === lid)) {
+      const teamsInLeague = this.context.state.teams_by_league.find(
+        x => x.league_id === lid
+      ).teams;
+      if (editedTeamSchedule[0].home_team_id !== this.state.id) {
+        const OppTeamIndex = teamsInLeague.findIndex(
+          x => x.id === editedTeamSchedule[0].home_team_id
+        );
+        game1Opp = teamsInLeague[OppTeamIndex].name;
+      } else if (editedTeamSchedule[0].away_team_id !== this.state.id) {
+        const OppTeamIndex = teamsInLeague.findIndex(
+          x => x.id === editedTeamSchedule[0].away_team_id
+        );
+        game1Opp = teamsInLeague[OppTeamIndex].name;
+      }
+      const game1 = new Date(editedTeamSchedule[0].start_time);
+      let options = { month: 'long' };
+      let game1Month = new Intl.DateTimeFormat('en-US', options).format(game1);
+      let game1Day = game1.getDate();
+      let game1Hours = game1.getHours();
+      let game1Minutes = game1.getMinutes();
+      if (game1Minutes < 10) {
+        game1Minutes = '0' + game1Minutes;
+      }
+      let game1Time = game1Hours + ':' + game1Minutes;
+      if (game1Hours < 12) {
+        game1Time += ' AM';
+      }
+      if (game1Hours > 12) {
+        game1Time += ' PM';
+      }
+
+      let scheduleGame1 = {
+        game1Month,
+        game1Day,
+        game1Time,
+        game1Opp
+      };
+      this.setState({
+        scheduleGame1
+      });
+    }
+
+    let game2Opp;
+
+    // Opponent Testing
+    if (this.context.state.teams_by_league.find(x => x.league_id === lid)) {
+      const teamsInLeague = this.context.state.teams_by_league.find(
+        x => x.league_id === lid
+      ).teams;
+      if (editedTeamSchedule[1].home_team_id !== this.state.id) {
+        const OppTeamIndex = teamsInLeague.findIndex(
+          x => x.id === editedTeamSchedule[1].home_team_id
+        );
+        game2Opp = teamsInLeague[OppTeamIndex].name;
+      } else if (editedTeamSchedule[1].away_team_id !== this.state.id) {
+        const OppTeamIndex = teamsInLeague.findIndex(
+          x => x.id === editedTeamSchedule[1].away_team_id
+        );
+        game2Opp = teamsInLeague[OppTeamIndex].name;
+      }
+      const game2 = new Date(editedTeamSchedule[1].start_time);
+      let options = { month: 'long' };
+
+      let game2Month = new Intl.DateTimeFormat('en-US', options).format(game2);
+      let game2Day = game2.getDate();
+      let game2Hours = game2.getHours();
+      let game2Minutes = game2.getMinutes();
+      if (game2Minutes < 10) {
+        game2Minutes = '0' + game2Minutes;
+      }
+      let game2Time = game2Hours + ':' + game2Minutes;
+      if (game2Hours < 12) {
+        game2Time += ' AM';
+      }
+      if (game2Hours > 12) {
+        game2Time += ' PM';
+      }
+
+      let scheduleGame2 = {
+        game2Month,
+        game2Day,
+        game2Time,
+        game2Opp
+      };
+      this.setState({
+        scheduleGame2
+      });
     }
   }
 
+  componentDidMount = async () => {
+    if (this.state.ties > 0) {
+      this.setState({
+        containsTies: true
+      });
+    }
+    await this.getTeamSchedule();
+    await this.displayGames();
+  };
+
   render() {
     const { classes } = this.props;
+    const {
+      name,
+      id,
+      coach_name,
+      coach_email,
+      coach_phone,
+      wins,
+      losses,
+      ties
+    } = this.state;
+
     //     let teamNameShort = this.state.name;
     // if(teamNameShort.length > 12) { teamNameShort = teamNameShort.substring(0,11)}
     // This will keep team name from Breaking Card styling by Showing Only first 12 characters for team Name without altering team name.
@@ -146,15 +311,15 @@ class TeamCard extends React.Component {
           >
             <CardContent className={classes.container}>
               <Typography className={classes.title}>
-                {this.state.name}
-                  <EditIcon onClick={this.ClickHandler} />
+                {name}
+                <EditIcon onClick={this.ClickHandler} />
               </Typography>
               <Typography className={classes.p}>
-                Name: {this.state.coach_name}
+                Name: {coach_name}
                 <br />
-                Email: {this.state.coach_email}
+                Email: {coach_email}
                 <br />
-                Phone #: {this.state.coach_phone}
+                Phone #: {coach_phone}
               </Typography>
               <Typography
                 className={classes.pos}
@@ -162,9 +327,9 @@ class TeamCard extends React.Component {
               >
                 Record:
                 <br />
-                Wins: {this.state.wins}
+                Wins: {wins}
                 <br />
-                Losses: {this.state.losses}
+                Losses: {losses}
               </Typography>
               <Typography
                 className={classes.pos}
@@ -172,18 +337,24 @@ class TeamCard extends React.Component {
               >
                 Record:
                 <br />
-                Wins: {this.state.wins}
+                Wins: {wins}
                 <br />
-                Losses: {this.state.losses}
+                Losses: {losses}
                 <br />
-                Ties: {this.state.ties}
+                Ties: {ties}
               </Typography>
               <Typography className={classes.upcoming}>
                 Upcoming:
                 <br />
-                July 20th 7pm vs Woodside Warriors
+                {this.state.scheduleGame1.game1Month}{' '}
+                {this.state.scheduleGame1.game1Day}{' '}
+                {this.state.scheduleGame1.game1Time} vs{' '}
+                {this.state.scheduleGame1.game1Opp}
                 <br />
-                Game 2
+                {this.state.scheduleGame2.game2Month}{' '}
+                {this.state.scheduleGame2.game2Day}{' '}
+                {this.state.scheduleGame2.game2Time} vs{' '}
+                {this.state.scheduleGame2.game2Opp}
               </Typography>
               {/* Still not quite Sure how we are going to do the upcoming games part. */}
             </CardContent>
@@ -199,7 +370,7 @@ class TeamCard extends React.Component {
                   fullWidth
                   className={classes.title}
                 >
-                  <InputLabel htmlFor="name">Team Name: {this.state.name}</InputLabel>
+                  <InputLabel htmlFor="name">Team Name: {name}</InputLabel>
                   <Input
                     id="name"
                     name="name"
@@ -209,7 +380,7 @@ class TeamCard extends React.Component {
                 </FormControl>
                 <FormControl margin="none" fullWidth>
                   <InputLabel htmlFor="coach_name">
-                    Coach Name: {this.state.coach_name}
+                    Coach Name: {coach_name}
                   </InputLabel>
                   <Input
                     id="coach_name"
@@ -219,7 +390,7 @@ class TeamCard extends React.Component {
                 </FormControl>
                 <FormControl margin="none" fullWidth>
                   <InputLabel htmlFor="coach_email">
-                    Coach Email: {this.state.coach_email}
+                    Coach Email: {coach_email}
                   </InputLabel>
                   <Input
                     id="coach_email"
@@ -229,7 +400,7 @@ class TeamCard extends React.Component {
                 </FormControl>
                 <FormControl margin="none">
                   <InputLabel htmlFor="coach_phone">
-                    Coach #: {this.state.coach_phone}
+                    Coach #: {coach_phone}
                   </InputLabel>
                   <Input
                     id="coach_phone"
@@ -238,11 +409,11 @@ class TeamCard extends React.Component {
                   />
                 </FormControl>
                 <FormControl margin="none">
-                  <InputLabel htmlFor="wins">Wins: {this.state.wins}</InputLabel>
+                  <InputLabel htmlFor="wins">Wins: {wins}</InputLabel>
                   <Input id="wins" name="wins" onChange={this.InputHandler} />
                 </FormControl>
                 <FormControl margin="none">
-                  <InputLabel htmlFor="losses">Losses: {this.state.losses}</InputLabel>
+                  <InputLabel htmlFor="losses">Losses: {losses}</InputLabel>
                   <Input
                     id="losses"
                     name="losses"
@@ -250,7 +421,7 @@ class TeamCard extends React.Component {
                   />
                 </FormControl>
                 <FormControl margin="none" display="none">
-                  <InputLabel htmlFor="ties">Ties: {this.state.ties}</InputLabel>
+                  <InputLabel htmlFor="ties">Ties: {ties}</InputLabel>
                   <Input id="ties" name="ties" onChange={this.InputHandler} />
                 </FormControl>
               </form>
