b2b01fb5dd914e1ce99a2b06589416ceba723e43
Jesse Anderson <janders9009@gmail.com>
2019-04-23 23:17:36 -0500

    Adjusting styles of admin cancellation view

--- frontend/src/components/Cancellations/AdminCancellationItem.js
+++ frontend/src/components/Cancellations/AdminCancellationItem.js
@@ -34,13 +34,16 @@ class AdminCancellationItem extends React.Component {
 
   render() {
     const { cancellation, classes, classname } = this.props;
+    const year = String(new Date(cancellation.start_time).getYear());
     return (
       <div className={classname}>
         <p className="cancellation-title">
           {cancellation.away_team_name} vs. {cancellation.home_team_name}
         </p>
         <p className="cancellation-time">
-          {cancellation.start_time} - {cancellation.end_time}
+          {new Date(cancellation.start_time).getMonth() + 1}/
+          {new Date(cancellation.start_time).getDate()}/
+          {'20' + year.substr(1, 2)}
         </p>
         <div className="btns">
           {!cancellation.acknowledged ? (
