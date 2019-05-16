b00d5ce790bcf6f3787e9c6c4553c7c63aca4da9
Jesse Anderson <janders9009@gmail.com>
2019-05-15 21:19:39 -0500

    returning items from database ordered by id

--- backend/data/models/teamModel.js
+++ backend/data/models/teamModel.js
@@ -2,26 +2,36 @@ const db = require('../dbConfig.js');
 
 module.exports = {
   getTeamsByUser: user => {
-    return db('team').where('coach_user_id', user.id);
+    return db('team')
+      .where('coach_user_id', user.id)
+      .orderBy('id');
   },
 
-  getTeamsByLeague: (leagueId) =>{
-      return db('team').where('league_id', leagueId);
+  getTeamsByLeague: leagueId => {
+    return db('team')
+      .where('league_id', leagueId)
+      .orderBy('id');
   },
 
-  getTeamById: (id) =>{
-      return db('team').where('id', id);
+  getTeamById: id => {
+    return db('team')
+      .where('id', id)
+      .orderBy('id');
   },
 
-  insert: (team) =>{
-      return db('team').insert(team, 'id');
+  insert: team => {
+    return db('team').insert(team, 'id');
   },
 
-  update: (id, team) =>{
-      return db('team').where('id', id).update(team);
+  update: (id, team) => {
+    return db('team')
+      .where('id', id)
+      .update(team);
   },
 
-  remove: (id) =>{
-      return db('team').where('id', id).del();
-  },
+  remove: id => {
+    return db('team')
+      .where('id', id)
+      .del();
+  }
 };
